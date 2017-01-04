0863ea5e16dfd1baf91e518bbd3a7fdfdbf8d3a8
zstrad44 <zachstradling@gmail.com>
2017-01-03 17:33:29 -0700

    added mailer, generated devise views. Still doesn't send email when I sign up.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,3 @@
+class UserMailer < ApplicationMailer
+  default from:"zachstradling@gmail.com"
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Cnfig for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
